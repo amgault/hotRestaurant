454190aecb610890bca0b77552e68c7fffb4c4ca
Amanda Gault <amandagault@dhcp-10-102-249-106.wireless.northwestern.private>
2017-08-05 12:09:20 -0500

    added get filepaths in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var PORT = 4000;
 
@@ -9,10 +10,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"))
+});
+
+app.get('/tables', function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"))
+});
+
+app.get('/reservation',function(req, res){
+    res.sendFile(path.join(__dirname, "reservation.html"))
+} );
 
 //define some routes
-add.post('/add', function(req,res){
-    if (req.body);
+app.post('/api/reservation', function(req,res){
+//    if (req.body);
+    console.log(req.body);
     
 });
 
